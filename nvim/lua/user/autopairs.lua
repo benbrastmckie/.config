b5e02f841c0c0ea3107d8369eba6a67dc4e9f0ef
benbrastmckie <credential.helper>
2022-11-17 17:16:37 +0530

    need to get lsp to import from zotero

--- nvim/lua/user/autopairs.lua
+++ nvim/lua/user/autopairs.lua
@@ -12,18 +12,31 @@ npairs.setup {
     java = false,
   },
   disable_filetype = { "TelescopePrompt", "spectre_panel" },
-  fast_wrap = {
-    map = "<M-e>",
-    chars = { "{", "[", "(", '"', "'" },
-    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-    offset = 0, -- Offset from pattern match
-    end_key = "$",
-    keys = "qwertyuiopzxcvbnmasdfghjkl",
-    check_comma = true,
-    highlight = "PmenuSel",
-    highlight_grey = "LineNr",
-  },
+  -- fast_wrap = {
+  --   map = "<M-e>",
+  --   chars = { "{", "[", "(", '"', "'" },
+  --   pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+  --   offset = 0, -- Offset from pattern match
+  --   end_key = "$",
+  --   keys = "qwertyuiopzxcvbnmasdfghjkl",
+  --   check_comma = true,
+  --   highlight = "PmenuSel",
+  --   highlight_grey = "LineNr",
+  -- },
 }
+
+local Rule = require('nvim-autopairs.rule')
+local npairs = require('nvim-autopairs')
+
+-- npairs.add_rule(Rule("$$","$$","tex"))
+npairs.add_rules({
+  Rule("$","$","tex"),
+  Rule("$ "," ","tex"),
+  Rule("[ "," ","tex"),
+  Rule("{ "," ","tex"),
+  Rule("( "," ","tex"),
+  }
+)
 
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
 local cmp_status_ok, cmp = pcall(require, "cmp")
---
+++ nvim/lua/user/vimtex.lua
@@ -0,0 +1,21 @@
+-- PDF Viewer:
+-- http://manpages.ubuntu.com/manpages/trusty/man5/zathurarc.5.html
+vim.g['vimtex_view_method'] = 'zathura'
+vim.g['vimtex_quickfix_mode'] =0
+
+-- Ignore mappings
+vim.g['vimtex_mappings_enabled'] = 0
+
+-- Error Suppression:
+-- https://github.com/lervag/vimtex/blob/master/doc/vimtex.txt
+
+vim.g['vimtex_log_ignore'] = ({
+  'Underfull',
+  'Overfull',
+  'specifier changed to',
+  'Token not allowed in a PDF string',
+}) 
+
+vim.g['vimtex_context_pdf_viewer'] = 1
+vim.g['vimtex_context_pdf_viewer'] = 'okular'
+
