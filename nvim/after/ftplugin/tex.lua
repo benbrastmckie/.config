6411dd4713f245e329af5374a9d562d79710789d
kylechui <kyle.chui+github@pm.me>
2022-11-19 20:00:59 -0800

    feat: Fix LaTeX surrounds.

--- nvim/after/ftplugin/tex.lua
+++ nvim/after/ftplugin/tex.lua
@@ -1,23 +1,19 @@
-local M = {}
-
-local config = require("nvim-surround.config")
-
 require("nvim-surround").buffer_setup({
   surrounds = {
     ["e"] = {
       add = function()
-        local env = require("nvim-surround.config").get_input "Environment: "
+        local env = require("nvim-surround.config").get_input("Environment: ")
         return { { "\\begin{" .. env .. "}" }, { "\\end{" .. env .. "}" } }
       end,
     },
     ["Q"] = {
       add = { "``", "''" },
-      find = "%b``''",
+      find = "``.-''",
       delete = "^(``)().-('')()$",
     },
     ["q"] = {
       add = { "`", "'" },
-      find = "%b`'",
+      find = "`.-'",
       delete = "^(`)().-(')()$",
     },
     ["b"] = {
@@ -56,13 +52,7 @@ require("nvim-surround").buffer_setup({
       delete = "^(\\%a-tt{)().-(})()$",
     },
     ["$"] = {
-        add = { "$ ", " $" },
-        find = function()
-            return M.get_selection({ motion = "a$" })
-        end,
-        delete = "^($)().-($)()$",
+      add = { "$ ", " $" },
     },
   },
 })
-
-return M
