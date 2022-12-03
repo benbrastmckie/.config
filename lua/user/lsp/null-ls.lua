5617a38000977ece178fa39cffdac21fdfbbefb8
benbrastmckie <credential.helper>
2022-12-03 12:55:05 +0530

    update

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -14,6 +14,7 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
+    -- null_ls.builtins.completion.spell,
     -- diagnostics.flake8
 	},
 })
--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -9,7 +9,7 @@ local setup = {
     registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
     spelling = {
       enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-      suggestions = 20, -- how many suggestions should be shown in the list?
+      suggestions = 10, -- how many suggestions should be shown in the list?
     },
     -- the presets plugin, adds help for a bunch of default keybindings in Neovim
     -- No actual key bindings are created
