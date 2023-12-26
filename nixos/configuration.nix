2d5bcbecd3cdc6ad3186e6bc5811e48ce34b5789
benbrastmckie <benbrastmckie@gmail.com>
2023-12-26 18:37:08 -0500

    update hostname

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nandi"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
