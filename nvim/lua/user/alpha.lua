bf8925cfd2a107635de12d7e41eac8a69197875b
benbrastmckie <benbrastmckie@gmail.com>
2024-01-15 20:53:11 -0500

    update

--- nvim/lua/user/alpha.lua
+++ nvim/lua/user/alpha.lua
@@ -20,10 +20,10 @@ dashboard.section.buttons.val = {
   -- dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
 	-- dashboard.button("d", "✗  Delete session", "<cmd>SessionsManager delete_session<CR>"),
   dashboard.button("s", "  Sessions", "<cmd>SessionManager load_session<CR>"),
-  dashboard.button("r", "  Recent", ":Telescope oldfiles <CR>"),
-	dashboard.button("f", "  Find", ":Telescope find_files <CR>"),
+  dashboard.button("r", "󰈚  Recent", ":Telescope oldfiles <CR>"),
+	dashboard.button("f", "  Find", ":Telescope find_files <CR>"),
 	dashboard.button("c", "  Config", ":e $MYVIMRC <CR>"),
-	dashboard.button("i", "  Info", "<cmd>e ~/.config/CheatSheet.md<cr>"),
+	dashboard.button("i", "  Info", "<cmd>e ~/.config/CheatSheet.md<cr>"),
 	dashboard.button("q", "✗  Quit", ":qa<CR>"),
 }
 
