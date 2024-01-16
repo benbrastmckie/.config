5604c8a31d43b55bae2f476c10b6ed122c19920c
benbrastmckie <benbrastmckie@gmail.com>
2024-01-16 15:51:05 -0500

    almost finished whichkey

--- nvim/lua/josean/plugins/nvim-tree.lua
+++ nvim/lua/josean/plugins/nvim-tree.lua
@@ -52,10 +52,6 @@ return {
     keymap.set('n', 'S',     api.tree.search_node,                  opts('Search'))
     keymap.set('n', 'x',     api.fs.cut,                            opts('Cut'))
     keymap.set('n', '<2-LeftMouse>',  api.node.open.edit,           opts('Open'))
-    keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    -- keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    -- keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    -- keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
 
     end
 
