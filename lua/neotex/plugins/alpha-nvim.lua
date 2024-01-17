40ff7f14a4f87f712252241c406aae52a275f5aa
benbrastmckie <benbrastmckie@gmail.com>
2024-01-16 21:35:23 -0500

    working on cmp

--- lua/neotex/plugins/alpha-nvim.lua
+++ lua/neotex/plugins/alpha-nvim.lua
@@ -26,7 +26,9 @@ return {
       dashboard.button("f", "  Find", ":Telescope find_files <CR>"),
       dashboard.button("c", "  Config", ":e $MYVIMRC <CR>"),
       dashboard.button("i", "  Info", "<cmd>e ~/.config/CheatSheet.md<cr>"),
-      dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
+      dashboard.button("p", "  Plugins", "<cmd>Lazy<cr>"),
+      dashboard.button("h", "  Checkhealth", "<cmd>checkhealth<cr>"),
+      dashboard.button("q", "  Quit", "<cmd>qa!<CR>"),
     }
 
     -- Send config to alpha
