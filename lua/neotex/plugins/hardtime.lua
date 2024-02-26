52ab061e149e1905c8cdc2b957d4013f7b626377
benbrastmckie <benbrastmckie@gmail.com>
2024-02-25 20:56:05 -0500

    added zen-mode and hardtime

---
+++ lua/neotex/plugins/hardtime.lua
@@ -0,0 +1,7 @@
+return {
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  opts = {
+    disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason" }, -- Add "oil" to the disabled_filetypes
+  }
+}
