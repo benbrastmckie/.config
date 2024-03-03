e1d5f04e0d4bc7d831fa0c176c6942418b96db95
benbrastmckie <benbrastmckie@gmail.com>
2024-03-03 13:32:45 -0500

    wrapped zoxide condition in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,9 @@ if status is-interactive
 end
 
 bind --erase \ct
-# bind \ct true
+# removes the mapping <C-t> which is being used to close the terminal in NeoVim
+
+if type -q zoxide
 zoxide init fish --cmd cd | source
+# removes the mapping <C-t> which is being used to close the terminal in NeoVim
+end
