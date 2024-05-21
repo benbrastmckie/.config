f83aa51e16bd949a5690da0c7e74d02b2809588d
benbrastmckie <benbrastmckie@gmail.com>
2024-05-21 18:08:08 -0400

    minor change

--- fish/config.fish
+++ fish/config.fish
@@ -6,9 +6,9 @@ end
 bind --erase \ct
 # bind \ct true
 
-# There's a catch 22 here. xodide is called but won't be found unless
-# fish is aware of the paths set by brew:.
-#    /opt/brew/bin/brew shellenv >> ~/.config/fish/config.fish # Ensures that brew paths are recognised inside fish
+# fish is aware of the paths set by brew:
+# to ensure that brew paths are recognized inside fish, run:
+#    /opt/brew/bin/brew shellenv >> ~/.config/fish/config.fish 
 
 # runs zoxide if installed
 if type -q zoxide
