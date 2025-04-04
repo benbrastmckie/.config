92ff0d5a0cb8b39a82c2be0cd8a7097e6f5a128d
benbrastmckie <benbrastmckie@gmail.com>
2025-04-03 21:25:10 -0400

    save avante_prefs and avante_commands

--- lua/neotex/core/init.lua
+++ lua/neotex/core/init.lua
@@ -2,3 +2,7 @@ require("neotex.core.autocmds")
 require("neotex.core.functions")
 require("neotex.core.keymaps")
 require("neotex.core.options")
+
+-- Set up Avante commands
+local avante_commands = require("neotex.core.avante_commands")
+avante_commands.setup_stop_command()
