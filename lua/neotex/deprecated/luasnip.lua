920736b0a22acbdca6f85468cdc4441bc094f262
benbrastmckie <benbrastmckie@gmail.com>
2025-05-10 12:01:00 -0400

    removed duplicates from deprecated/

--- lua/neotex/deprecated/luasnip.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "L3MON4D3/LuaSnip",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("luasnip.loaders.from_snipmate").load({ paths = "~/.config/nvim/snippets/" })
-  end
-}
--- lua/neotex/deprecated/mini.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'echasnovski/mini.nvim',
-  version = false,
-  config = function()
-    require('mini.icons').setup()
-  end
-}
-
