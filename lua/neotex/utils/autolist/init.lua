f198b90b438117ae63d0c2c1c78e84b0c222ff70
benbrastmckie <benbrastmckie@gmail.com>
2025-05-15 16:02:46 -0400

    refactored autolist

---
+++ lua/neotex/utils/autolist/init.lua
@@ -0,0 +1,8 @@
+-- Main autolist module with exported functions
+local M = {}
+
+-- Export the operations and utils modules
+M.operations = require("neotex.utils.autolist.list_operations")
+M.utils = require("neotex.utils.autolist.utils")
+
+return M
